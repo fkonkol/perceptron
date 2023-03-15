58d653770fdbedf4a612cea8ab409fcb73fd492c
fkonkol <kaenel1337@gmail.com>
2023-03-15 19:24:58 +0100

    feat: implement data feeding

--- main.c
+++ main.c
@@ -1,8 +1,30 @@
 #include <stdio.h>
 
+#define WIDTH 20
+#define HEIGHT 20
+
+typedef float layer_t[HEIGHT][WIDTH];
+
+float feed_forward(layer_t inputs, layer_t weights)
+{
+    float output = 0.0f;
+
+    for (int y = 0; y < HEIGHT; ++y) {
+        for (int x = 0; x < WIDTH; ++x) {
+            output += inputs[y][x] * weights[y][x];
+        }
+    }
+
+    return output;
+}
+
+static layer_t inputs;
+static layer_t weights;
+
 int main(void)
 {
-    printf("hello, world\n");
+    float output = feed_forward(inputs, weights);
+    printf("output = %f\n", output);
 
     return 0;
 }
